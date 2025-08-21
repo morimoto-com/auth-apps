f70a33c259531070ef7dc57645b56564e20b88ab
morishin <morimoto820@yahoo.co.jp>
2025-08-22 07:04:12 +0900

    javaアプリ追加中

---
+++ backend/authjava/src/test/java/com/authapps/authjava/AuthjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authapps.authjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
